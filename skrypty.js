c2990a880a6b0a05588bc6e67780dcc29eb68f1c
Oliwier Stępniak <oliwierstepniak@gmial.com>
2021-05-08 21:46:18 +0200

    Wypisanie wszystkich liter na stronie

--- skrypty.js
+++ skrypty.js
@@ -15,4 +15,57 @@ function wypiszHaslo(){
     document.getElementById("plansza").innerHTML = hasloZakodowane;
 }
 
-window.onload = wypiszHaslo;
+var litery = new Array(35);
+litery[0] = "A";
+litery[1] = "Ą";
+litery[2] = "B";
+litery[3] = "C";
+litery[4] = "Ć";
+litery[5] = "D";
+litery[6] = "E";
+litery[7] = "Ę";
+litery[8] = "F";
+litery[9] = "G";
+litery[10] = "H";
+litery[11] = "I";
+litery[12] = "J";
+litery[13] = "K";
+litery[14] = "L";
+litery[15] = "Ł";
+litery[16] = "M";
+litery[17] = "N";
+litery[18] = "Ń";
+litery[19] = "O";
+litery[20] = "Ó";
+litery[21] = "P";
+litery[22] = "Q";
+litery[23] = "R";
+litery[24] = "S";
+litery[25] = "Ś";
+litery[26] = "T";
+litery[27] = "U";
+litery[28] = "V";
+litery[29] = "W";
+litery[30] = "X";
+litery[31] = "Y";
+litery[32] = "Z";
+litery[33] = "Ż";
+litery[34] = "Ź";
+
+
+function wypisanieLiterek(){
+
+    var trescDiva ="";
+
+    for(var i = 0; i <= 34; i++){
+        trescDiva += '<div class="litera">'+litery[i]+'</div>';
+        if((i+1) % 7 == 0) trescDiva = trescDiva + '<div style="clear: both;"></div>';
+    }
+
+    document.getElementById("alfabet").innerHTML = trescDiva;
+}
+
+window.onload = ()=>{
+    wypiszHaslo();
+    wypisanieLiterek();
+};
